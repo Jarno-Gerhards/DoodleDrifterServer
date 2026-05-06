836ff53eeeeca0516b7c3cce811aa91acf3235da
Jarno Gerhards <j.gerhards.edu@gmail.com>
2026-05-06 09:47:06 +0200

    Enhance WebSocket message logging
    Updated WebSocket message handling with logging.

--- server.js
+++ server.js
@@ -25,71 +25,82 @@ wss.on("connection", (ws) => {
   console.log("🔥 WebSocket CLIENT CONNECTED");
 
   //console.log("RAW MESSAGE:", msg.toString());
-
-  ws.on("message", (msg) => {
-    let data;
+ws.on("message", (msg) => {
+    console.log("RAW TYPE:", typeof msg);
+    console.log("RAW BUFFER:", msg);
+    console.log("STRING:", msg.toString());
 
     try {
-      data = JSON.parse(msg);
-    } catch {
-      return;
+        const parsed = JSON.parse(msg.toString());
+        console.log("PARSED OK:", parsed);
+    } catch (e) {
+        console.log("❌ JSON FAIL");
     }
+});
+  // ws.on("message", (msg) => {
+  //   let data;
 
-    switch (data.type) {
+  //   try {
+  //     data = JSON.parse(msg);
+  //   } catch {
+  //     return;
+  //   }
 
-      case "HOST_CONNECT":
-        host = ws;
-        console.log("Host connected");
-        break;
+  //   switch (data.type) {
 
-      case "JOIN":
-        const id = "p" + Date.now();
-        players[id] = ws;
-        ws.playerId = id;
+  //     case "HOST_CONNECT":
+  //       host = ws;
+  //       console.log("Host connected");
+  //       break;
 
-        console.log("Player joined:", id);
+  //     case "JOIN":
+  //       const id = "p" + Date.now();
+  //       players[id] = ws;
+  //       ws.playerId = id;
 
-        if (host) {
-          host.send(JSON.stringify({
-            type: "PLAYER_JOINED",
-            playerId: id,
-            name: data.name
-          }));
-        }
-        break;
+  //       console.log("Player joined:", id);
 
-      case "SUBMIT_DRAWING":
-        console.log("Drawing received");
+  //       if (host) {
+  //         host.send(JSON.stringify({
+  //           type: "PLAYER_JOINED",
+  //           playerId: id,
+  //           name: data.name
+  //         }));
+  //       }
+  //       break;
 
-        if (host) {
-          host.send(JSON.stringify({
-            type: "DRAWING_SUBMITTED",
-            playerId: ws.playerId,
-            image: data.image
-          }));
-        }
-        break;
+  //     case "SUBMIT_DRAWING":
+  //       console.log("Drawing received");
 
-      case "VOTE":
-        console.log("Vote received");
+  //       if (host) {
+  //         host.send(JSON.stringify({
+  //           type: "DRAWING_SUBMITTED",
+  //           playerId: ws.playerId,
+  //           image: data.image
+  //         }));
+  //       }
+  //       break;
 
-        if (host) {
-          host.send(JSON.stringify({
-            type: "VOTE_SUBMITTED",
-            playerId: ws.playerId,
-            targetId: data.drawingId
-          }));
-        }
-        break;
+  //     case "VOTE":
+  //       console.log("Vote received");
 
-      case "START_DRAW":
-        broadcast({ type: "STATE", state: "DRAW" });
-        break;
+  //       if (host) {
+  //         host.send(JSON.stringify({
+  //           type: "VOTE_SUBMITTED",
+  //           playerId: ws.playerId,
+  //           targetId: data.drawingId
+  //         }));
+  //       }
+  //       break;
 
-      case "START_VOTE":
-        broadcast({ type: "STATE", state: "VOTE" });
-        break;
-    }
+  //     case "START_DRAW":
+  //       broadcast({ type: "STATE", state: "DRAW" });
+  //       break;
+
+  //     case "START_VOTE":
+  //       broadcast({ type: "STATE", state: "VOTE" });
+  //       break;
+  //   }
   });
 
   ws.on("close", () => {
